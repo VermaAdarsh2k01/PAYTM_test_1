a247737a00d0f3c26695b01a18a0f3c603a679c3
Adarsh Verma <verma.adarsh2510@gmail.com>
2024-05-17 14:05:50 +0530

    changes

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -42,7 +42,7 @@ export const SignUp = () => {
                     <Button 
                         onClick={async () => {
                             try{
-                                const response = await axios.post("http://192.168.29.189:3000/api/v1/user/signup",{
+                                const response = await axios.post("https://paytm-test-1-1.onrender.com/api/v1/user/signup",{
                                 username,
                                 firstName,
                                 lastName,
