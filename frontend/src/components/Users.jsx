22aae8f0d76451b45accddfe2e96495e8b6a556d
Adarsh Verma <verma.adarsh2510@gmail.com>
2024-05-17 14:22:52 +0530

    changes

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -10,7 +10,7 @@ export const Users = () => {
     const [filter , setFilter] = useState("");
 
     useEffect( ()=> {
-        axios.get("http://192.168.29.189:3000/api/v1/user/bulk?filter=" + filter)
+        axios.get("https://paytm-test-1-1.onrender.com/api/v1/user/bulk?filter=" + filter)
             .then( response => {
                 setUsers(response.data.user)
             }) 
